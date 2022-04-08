79f28dbfe60b7a7a3ad255969f7791a4f742d12e
Thom Harty <thomharty@192.168.1.102>
2022-04-08 17:35:35 +0100

    Passed Controller Tests

--- src/User.service.ts
+++ src/User.service.ts
@@ -2,7 +2,7 @@ import { UserRequestDto } from './Database';
 import { UserRepository } from './User.repository';
 
 export class UserService {
-  constructor(public userRepo: UserRepository) {}
+  constructor(public readonly userRepo: UserRepository) {}
 
   async createUser(userData: UserRequestDto) {
     return this.userRepo.createUser(userData);
