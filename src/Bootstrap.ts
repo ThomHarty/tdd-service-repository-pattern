3aaab71cd1c8cc6b8d2505ee960df0db4af4a0c8
Thom Harty <thomharty@192.168.1.102>
2022-04-08 17:44:17 +0100

    Passed Bootstrap Tests

--- src/Bootstrap.ts
+++ src/Bootstrap.ts
@@ -1,1 +1,7 @@
-import 'dotenv/config';
+import { UserController } from './User.controller';
+import { UserRepository } from './User.repository';
+import { UserService } from './User.service';
+
+const userRepo = new UserRepository();
+const userService = new UserService(userRepo);
+export const userController = new UserController(userService);
